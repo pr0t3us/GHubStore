abcb0343f077ac53a68c8ae7cbad8468f6a9fe67
Serghei Oleinicenco <serghei.oleinicenco@ellation.com>
2022-06-25 18:10:40 +0300

    Add Logger for debug build

--- app/src/main/java/com/androidpositive/ghubstore/GHubStoreApplication.kt
+++ app/src/main/java/com/androidpositive/ghubstore/GHubStoreApplication.kt
@@ -1,7 +1,17 @@
 package com.androidpositive.ghubstore
 
 import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GHubStoreApplication: Application()
+class GHubStoreApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Logger.addLogAdapter(AndroidLogAdapter())
+        }
+    }
+}
+
--- app/src/main/java/com/androidpositive/ghubstore/presentation/repositorylist/RepositoryListViewModel.kt
+++ app/src/main/java/com/androidpositive/ghubstore/presentation/repositorylist/RepositoryListViewModel.kt
@@ -1,4 +1,4 @@
-package com.androidpositive.ghubstore.presentation.home
+package com.androidpositive.ghubstore.presentation.repositorylist
 
 import com.androidpositive.viewmodel.ObservableViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,5 +9,10 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val githubClient: GitHub
 ) : ObservableViewModel() {
+    private val repositoryName: String = "pr0t3us/GHubStore"
+    init {
+        val repository = githubClient.getRepository(repositoryName)
+        val releases = repository.listReleases()
 
+    }
 }
